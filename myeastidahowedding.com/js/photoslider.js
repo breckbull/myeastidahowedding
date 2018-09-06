4bc39ab3bffbbb6b14c8da14044c97054b6d6b50
Kylie Shelton <35777969+causticjellyfish@users.noreply.github.com>
2018-09-06 08:06:48 -0600

    Add files via upload

---
+++ myeastidahowedding.com/js/photoslider.js
@@ -0,0 +1,43 @@
+var slideIndex = 1;
+showSlides();
+
+// next/previous
+function plusSlidesBack(n) {
+	showSlidesC(slideIndex -= n);
+}
+
+function plusSlides(n) {
+	showSlidesC(slideIndex += n);
+}
+
+// images
+// function currentSlide(n) {
+// 	showSlides(slideIndex = n);
+// }
+
+function showSlidesC(n) {
+	var i;
+	var slides = document.getElementsByClassName("weddingpic");
+	if (n > slides.length) {slideIndex = 1}
+	if (n < 1) {slideIndex = slides.length}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex-1].style.display = "block";
+}
+
+// automatic cycle
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("weddingpic");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 3000);
+}
